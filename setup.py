d8dfafa2eb21124997b9074c1fc85d7cb9426271
c3-sravanj <139167151+c3-sravanj@users.noreply.github.com>
2023-12-03 17:05:37 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,6 @@ import shutil
 from os import path
 from setuptools import find_packages, setup
 from typing import List
-import torch
-from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
-from torch.utils.hipify import hipify_python
-
-torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
-assert torch_ver >= [1, 6], "Requires PyTorch >= 1.6"
-
 
 def get_version():
     init_py_path = path.join(path.abspath(path.dirname(__file__)), "detectron2", "__init__.py")
@@ -36,117 +29,6 @@ def get_version():
         with open(init_py_path, "w") as f:
             f.write("".join(new_init_py))
     return version
-
-
-def get_extensions():
-    this_dir = path.dirname(path.abspath(__file__))
-    extensions_dir = path.join(this_dir, "detectron2", "layers", "csrc")
-
-    main_source = path.join(extensions_dir, "vision.cpp")
-    sources = glob.glob(path.join(extensions_dir, "**", "*.cpp"))
-
-    from torch.utils.cpp_extension import ROCM_HOME
-
-    is_rocm_pytorch = (
-        True if ((torch.version.hip is not None) and (ROCM_HOME is not None)) else False
-    )
-
-    hipify_ver = (
-        [int(x) for x in torch.utils.hipify.__version__.split(".")]
-        if hasattr(torch.utils.hipify, "__version__")
-        else [0, 0, 0]
-    )
-
-    if is_rocm_pytorch and hipify_ver < [1, 0, 0]:
-
-        # Earlier versions of hipification and extension modules were not
-        # transparent, i.e. would require an explicit call to hipify, and the
-        # hipification would introduce "hip" subdirectories, possibly changing
-        # the relationship between source and header files.
-        # This path is maintained for backwards compatibility.
-
-        hipify_python.hipify(
-            project_directory=this_dir,
-            output_directory=this_dir,
-            includes="/detectron2/layers/csrc/*",
-            show_detailed=True,
-            is_pytorch_extension=True,
-        )
-
-        source_cuda = glob.glob(path.join(extensions_dir, "**", "hip", "*.hip")) + glob.glob(
-            path.join(extensions_dir, "hip", "*.hip")
-        )
-
-        shutil.copy(
-            "detectron2/layers/csrc/box_iou_rotated/box_iou_rotated_utils.h",
-            "detectron2/layers/csrc/box_iou_rotated/hip/box_iou_rotated_utils.h",
-        )
-        shutil.copy(
-            "detectron2/layers/csrc/deformable/deform_conv.h",
-            "detectron2/layers/csrc/deformable/hip/deform_conv.h",
-        )
-
-        sources = [main_source] + sources
-        sources = [
-            s
-            for s in sources
-            if not is_rocm_pytorch or torch_ver < [1, 7] or not s.endswith("hip/vision.cpp")
-        ]
-
-    else:
-
-        # common code between cuda and rocm platforms,
-        # for hipify version [1,0,0] and later.
-
-        source_cuda = glob.glob(path.join(extensions_dir, "**", "*.cu")) + glob.glob(
-            path.join(extensions_dir, "*.cu")
-        )
-
-        sources = [main_source] + sources
-
-    extension = CppExtension
-
-    extra_compile_args = {"cxx": []}
-    define_macros = []
-
-    if (torch.cuda.is_available() and ((CUDA_HOME is not None) or is_rocm_pytorch)) or os.getenv(
-        "FORCE_CUDA", "0"
-    ) == "1":
-        extension = CUDAExtension
-        sources += source_cuda
-
-        if not is_rocm_pytorch:
-            define_macros += [("WITH_CUDA", None)]
-            extra_compile_args["nvcc"] = [
-                "-O3",
-                "-DCUDA_HAS_FP16=1",
-                "-D__CUDA_NO_HALF_OPERATORS__",
-                "-D__CUDA_NO_HALF_CONVERSIONS__",
-                "-D__CUDA_NO_HALF2_OPERATORS__",
-            ]
-        else:
-            define_macros += [("WITH_HIP", None)]
-            extra_compile_args["nvcc"] = []
-
-        if torch_ver < [1, 7]:
-            # supported by https://github.com/pytorch/pytorch/pull/43931
-            CC = os.environ.get("CC", None)
-            if CC is not None:
-                extra_compile_args["nvcc"].append("-ccbin={}".format(CC))
-
-    include_dirs = [extensions_dir]
-
-    ext_modules = [
-        extension(
-            "detectron2._C",
-            sources,
-            include_dirs=include_dirs,
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ]
-
-    return ext_modules
 
 
 def get_model_zoo_configs() -> List[str]:
@@ -234,6 +116,4 @@ setup(
             "flake8-comprehensions",
         ],
     },
-    ext_modules=get_extensions(),
-    cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
 )
